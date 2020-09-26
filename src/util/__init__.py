e3fa66a493550433e26e57b731f42c65e1e17571
adrianmarino <adrianmarino@gmail.com>
2020-09-26 11:18:03 -0300

    feat: plot embedings

--- src/util/__init__.py
+++ src/util/__init__.py
@@ -2,3 +2,4 @@ from .data_utils import dataframe_to_dataset
 from .env_utils import tf_detected_devices, tf_version
 from .metrics import root_mean_squared_error, root_mean_squared_error, MetricMeterBuilder, diff_indicator
 from .plot import plot_model
+from .pca_plot_builder import PCAPlotBuilder
