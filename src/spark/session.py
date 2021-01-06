11bc474f52fa9fd5d4c1461de468ecccb6a82ce2
adrianmarino <adrianmarino@gmail.com>
2021-01-06 18:39:54 -0300

    feat: adjust model to run with a rtx 3080

--- src/spark/session.py
+++ src/spark/session.py
@@ -11,14 +11,15 @@ class SparkSessionFactory:
             .master("local[*]") \
             .appName("recommendations") \
             .config('spark.executor.resource.gpu.amount', '1') \
-            .config('spark.driver.memory', '8G') \
+            .config('spark.driver.memory', '6G') \
+            .config('spark.executor.memory', '4G') \
+            .config("spark.driver.extraClassPath", '/opt/apache-spark/jars/;/opt/apachrapids-4-spark_2.12-0.2.0.jar;e-spark/jars/cudf-0.17.jar') \
+            .config('spark.rapids.sql.incompatibleOps.enabled', 'true') \
+            .config('spark.rapids.sql.enabled', 'true') \
+            .config('spark.rapids.sql.explain', 'ALL') \
+            .config('spark.executor.instances', 12) \
             .getOrCreate()
-
-            #.config("spark.driver.extraClassPath",
-            #        '/opt/apache-spark/jars/rapids-4-spark_2.12-0.2.0.jar;/opt/apache-spark/jars/cudf-0.15-cuda11.jar') \
-            # .config('spark.rapids.sql.incompatibleOps.enabled', 'true') \
-            # .config('spark.rapids.sql.enabled', 'true') \
-            # .config('spark.rapids.sql.explain', 'ALL') \
+        
         session.sparkContext.setLogLevel("ERROR")
     
         return session
